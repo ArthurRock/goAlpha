c59bf3e3599fdc378ce12ce6f19a990eaefd1aa1
Miguel Arturo Martinez Martinez <roedor1percent@gmail.com>
2023-08-14 13:14:38 -0600

    Update Go

--- ejercicios/ejercicio01.go
+++ ejercicios/ejercicio01.go
@@ -1,24 +1,21 @@
 package ejercicios
 
 import (
-	"fmt"
 	"strconv"
 )
 
-var rst string
-var num int
-
 func Ejec(cad string) (int, string) {
 	var err error
-	num, err = strconv.Atoi(cad)
+	num, err := strconv.Atoi(cad)
 
-	fmt.Println(err)
-
-	if num > 100 {
-		rst = "Es mayor a 100"
-	} else {
-		rst = "Es menor a 100"
+	if err != nil {
+		return 0, "Hubo un error " + err.Error()
 	}
 
-	return num, rst
+	if num > 100 {
+		return num, "Es mayor a 100"
+	} else {
+		return num, "Es menor a 100"
+	}
+
 }
